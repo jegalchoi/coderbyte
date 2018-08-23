51e55525a20bbcbd90cf3f176904ec838bb8656c
jegalchoi <thefreshbegal@gmail.com>
2018-08-22 18:05:41 -0700

    Add vowel_count.html and vowel_count.js

---
+++ vowel_count.js
@@ -0,0 +1,15 @@
+let numberOfVowels = 0;
+
+function vowelCount (str) {
+  for(i = 0; i < str.length; i++) {
+    if(str.charAt(i) == "a" ||
+      str.charAt(i) == "e" ||
+      str.charAt(i) == "i" ||
+      str.charAt(i) == "o" ||
+      str.charAt(i) == "u"
+    ) {
+      numberOfVowels += 1
+    };
+  };
+  return numberOfVowels;
+};
